e0f040b3739e0901f3ed4b48020dc6ceea981146
dimovich <dimovich@gmail.com>
2018-07-10 23:45:54 +0300

    cljs-http stuff...

--- src/cljs/occupy/core.cljs
+++ src/cljs/occupy/core.cljs
@@ -1,18 +1,52 @@
 (ns occupy.core
-  (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [cljs-http.client :as http]
-            [cljs.core.async :as async :refer [<!]]
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
+  (:require [taoensso.timbre :refer [info]]
+            [cljs-http.client :as http]
+            [dommy.core :as d]
+            [cljs.core.async :as async :refer [<! >! chan]]
             [occupy.links :refer [dashboard-url]]))
 
 
-(def data (atom nil))
+
 
 (defn get-url [url]
+  (info "getting" url)
+  (let [c (chan)]
+    (go
+      (let [{data :body} (<! (http/get url {:with-credentials? false}))]
+        (>! c data)))
+    c))
+
+
+
+(defn coerce-article [data]
+  (-> data
+      (clojure.string/split #"\r\n")))
+
+
+
+(defn append-article [article]
+  (let [el (d/create-element :pre)]
+    (d/set-text! el article)
+    (-> (d/sel1 :#occupy-grid)
+        (d/append! el))))
+
+
+
+
+(defn fetch-articles [starting-url]
   (go
-    (reset! data
-            (-> (<! (http/get url {:with-credentials? false}))
-                :body
-                (clojure.string/split #"\r\n")))))
+    (let [article-urls (-> (<! (get-url starting-url))
+                           (clojure.string/split #"\r\n"))]
+
+      (info "article urls" article-urls)
+
+      (let [article-chans (map get-url article-urls)]
+        (while true
+          (let [article (async/alts! article-chans)]
+            (info "got article" article)
+            (-> article append-article)))))))
+
 
 
 (defn ^:export -main [& args])
