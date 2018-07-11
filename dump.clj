138e7a3389e0eda4eaa70df52bf54788bfb9bdf8
dimovich <dimovich@gmail.com>
2018-07-11 08:52:18 +0300

    ...

--- dump.clj
+++ dump.clj
@@ -1,3 +1,5 @@
+
+
 {:status 200, :success true, :body "De ce este nevoie\tCantitatea\tData\r\nPlapume\t2\t", :headers {"date" "Mon, 09 Jul 2018 22:02:07 GMT", "content-encoding" "gzip", "content-type" "text/tab-separated-values", "server" "GSE", "cache-control" "private, max-age=300", "content-disposition" "attachment; filename=\"Lucrurinecesare-Sheet1.tsv\"; filename*=UTF-8''Lucruri%20necesare%20-%20Sheet1.tsv", "expires" "Mon, 09 Jul 2018 22:02:07 GMT"}, :trace-redirects ["https://docs.google.com/spreadsheets/d/e/2PACX-1vSIGEoy5cTv8z_LQ8lDTSmpUbZ1PfuZbGqRZ2-7CZhVUIr9jokXoFBMYGUnnDL3cF3NSaAlwcrc_8vs/pub?gid=0&single=true&output=tsv" "https://docs.google.com/spreadsheets/d/e/2PACX-1vSIGEoy5cTv8z_LQ8lDTSmpUbZ1PfuZbGqRZ2-7CZhVUIr9jokXoFBMYGUnnDL3cF3NSaAlwcrc_8vs/pub?gid=0&single=true&output=tsv"], :error-code :no-error, :error-text ""}
 
 
